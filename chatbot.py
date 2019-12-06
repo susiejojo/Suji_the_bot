7ef23cccfd7f7e93aa44d0db431d6d461297193c
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-12-07 04:47:25 +0530

    Changes

--- chatbot.py
+++ chatbot.py
@@ -11,8 +11,11 @@ import string
 import webbrowser
 import time
 import requests
+import datetime
+import os
+import pandas as pd
 def warn(*args, **kwargs):
-    pass
+	pass
 import warnings
 warnings.warn = warn
 
@@ -24,11 +27,19 @@ def get_joke():
   data = resp.json()
   print(data["value"]["joke"])
 
-
-
-# In[39]:
-
-
+import praw
+import config
+def meme():
+	reddit = praw.Reddit(client_id = config.client_id, 
+						 client_secret = config.client_secret, 
+						 user_agent = config.user_agent)
+	subreddit = reddit.subreddit('ProgrammerHumor') 
+	posts = subreddit.top(limit=100)
+	image_urls=[]
+	for post in posts:
+	  image_urls.append(post.url)
+	webbrowser.get().open(image_urls[random.randint(1,99)])
+	return
 f=open('chatbot.txt','r',errors = 'ignore')
 
 
@@ -52,8 +63,15 @@ def LemTokens(tokens):
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 def LemNormalize(text):
 	return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
-
-
+def gsearch(text):
+	try: 
+	    from googlesearch import search 
+	except ImportError:  
+	    print("No module named 'google' found") 
+	  
+	for j in search(text, tld="com", num=10, stop=5, pause=1): 
+	    print(j)
+	return  
 # In[42]:
 
 
@@ -65,12 +83,18 @@ def LemNormalize(text):
 
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey")
 GREETING_RESPONSES = ["hi", "hey", "how're you", "hi there", "hello", "I am glad you are talking to me!"]
-COMMAND_INPUTS = ("jokes","youtube")
+COMMAND_INPUTS = ("jokes","youtube","meme")
 COMMAND_OUTPUTS = ["on it!...","hang on a minute..."]
 def greeting(sentence):
 	for word in sentence.split():
 		if word.lower() in GREETING_INPUTS:
 			return GREETING_RESPONSES[random.randint(0,5)]
+def checker(sentence):
+	for word in sentence.split():
+		if word.lower() in COMMAND_INPUTS:
+			return True
+		else:
+			return False
 def command(sentence,flag1):
 	for word in sentence.split():
 		if word.lower() in COMMAND_INPUTS:
@@ -78,14 +102,20 @@ def command(sentence,flag1):
 				if (flag1 == False):
 					print ("SUJI: ",COMMAND_OUTPUTS[random.randint(0,1)])
 					webbrowser.get().open("https://www.youtube.com/watch?v=G4I6PhHbH7o&list=RDG4I6PhHbH7o&start_radio=1")
-					flag1 = True
+					#flag1 = True
 					time.sleep(5)
 			if (word in COMMAND_INPUTS[0]):
 				if (flag1 == False):
 					print ("SUJI: ",COMMAND_OUTPUTS[random.randint(0,1)])
 					get_joke()
-					flag1 = True
-					#time.sleep(5)
+					#flag1 = True
+					time.sleep(5)
+			if (word in COMMAND_INPUTS[2]):
+				if (flag1 == False):
+					print ("SUJI: ",COMMAND_OUTPUTS[random.randint(0,1)])
+					meme()
+					#flag1 = True
+					time.sleep(5)
 			return COMMAND_OUTPUTS[random.randint(0,1)]
 
 
@@ -112,11 +142,13 @@ def response(user_response):
 	req_tfidf = flat[-2]
 	if(req_tfidf==0):
 		#if (flag1 !=False):
-		robo_response=robo_response+"I don't know this. Sorry!"
-		return robo_response
+		robo_response=robo_response+"I don't know this. Sorry! Do you want me to Google it?"
+		nflag = 1
+		return nflag,robo_response
 	else:
 		robo_response = robo_response+sent_tokens[idx]
-		return robo_response
+		nflag = 0
+		return nflag,robo_response
 
 
 # In[48]:
@@ -124,9 +156,13 @@ def response(user_response):
 
 flag=True
 flag1 = True
-print("SUJI: My name is Suji. I will answer your queries. Meow! You can also command me to carry out a few instructions!")
+print("SUJI: My name is Suji. I will answer your queries. Meow! You can also command me to carry out a few instructions! Let's start with your name.")
+print ("YOU: ",end="")
+username = input()
+nflag = 0
+print("SUJI: Hello ,",username,". I will remember to call you that. Go ahead...ask me something!")
 while(flag==True):
-	print ("YOU: ",end="")
+	print (username,": ",end="")
 	user_response = input()
 	user_response=user_response.lower()
 	if (user_response=='bye' or user_response=='goodbye' or user_response=='goodnight'):
@@ -143,13 +179,22 @@ while(flag==True):
 		else:
 			if (greeting(user_response)!=None):
 				print("SUJI: "+greeting(user_response))
-			elif (command(user_response,flag1)!=None):
-				#print ("Meow")
+			elif (checker(user_response)):
 				flag1 = False
 				command(user_response,flag1)
 			else:
 				print("SUJI: ",end="")
-				print(response(user_response))
+				# p = response(user_response)
+				fl,res = response(user_response)
+				print(res)
+				#print (fl)
+				if (fl==1):
+					print (username,": ",end="")
+					u_response = input()
+					u_response = u_response.lower()
+					if (u_response=='yes'):
+						print ("SUJI: Here are the top results for your search:")
+						gsearch(user_response)
 				sent_tokens.remove(user_response)
 
 
